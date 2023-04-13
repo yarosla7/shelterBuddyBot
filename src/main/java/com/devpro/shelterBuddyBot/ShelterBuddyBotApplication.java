684c447fa8e11a0f9c800e56b5450ae72dd337ba
y.yanin <yanin.yarosla7@gmail.com>
2023-04-13 21:30:15 +0300

    first commit

---
+++ src/main/java/com/devpro/shelterBuddyBot/ShelterBuddyBotApplication.java
@@ -0,0 +1,13 @@
+package com.devpro.shelterBuddyBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterBuddyBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelterBuddyBotApplication.class, args);
+    }
+
+}
