7dd86d7a50538cabfc1c9c2784246b99029e0b6e
y.yanin <yanin.yarosla7@gmail.com>
2023-04-13 22:27:32 +0300

    Этап 0. Бот научился приветствовать юзера. Добавил комментарии к коду.

--- src/main/java/com/devpro/shelterBuddyBot/listner/TelegramBotUpdatesListener.java
+++ src/main/java/com/devpro/shelterBuddyBot/listner/TelegramBotUpdatesListener.java
@@ -31,25 +31,30 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @Override
     public int process(List<Update> updates) {
-        try {
-            updates.forEach(update -> {
-                logger.info("Handles update: {}", update);
-            });
-
+        try { // Обходим все пришедшие апдейты и выводим их в лог updates.forEach(update -> { logger.info("Handles update: {}", update); });
+            // Получаем первое сообщение из списка пришедших апдейтов
             Message message = updates.get(0).message();
+            // Получаем ID чата, куда нужно отправлять сообщения
             Long chatId = message.chat().id();
+            // Получаем текст этого сообщения
             String text = message.text();
 
+            // Если текст первого сообщения "/start", отправляем приветственное сообщение
             if ("/start".equals(text)){
+                // Создаем объект класса SendMessage и передаем ему ID чата и текст сообщения
                 SendMessage sendMessage = new SendMessage(chatId, "Hello!");
+                // Отправляем сообщение через метод execute объекта класса TelegramBot
                 SendResponse sendResponse = telegramBot.execute(sendMessage);
+                // Если сообщение отправлено не успешно, выводим ошибку в лог
                 if (!sendResponse.isOk()) {
                     logger.error("Error during sending message: {}", sendResponse.description());
                 }
             }
         } catch (Exception e) {
+            // Если произошла ошибка в процессе обработки сообщения, выводим ее в лог
             logger.error(e.getMessage(), e);
         }
+// Возвращаем CONFIRMED_UPDATES_ALL, чтобы сообщить, что все апдейты обработаны
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 }
