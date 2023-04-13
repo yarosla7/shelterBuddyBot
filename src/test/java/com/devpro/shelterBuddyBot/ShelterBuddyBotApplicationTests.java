b273b11dcbf49096bffc5b98b8ac1d78ca6b35e7
y.yanin <yanin.yarosla7@gmail.com>
2023-04-13 21:30:15 +0300

    first commit

---
+++ src/test/java/com/devpro/shelterBuddyBot/ShelterBuddyBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpro.shelterBuddyBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelterBuddyBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
