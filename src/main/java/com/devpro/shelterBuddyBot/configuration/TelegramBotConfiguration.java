4aa46ec3e3a7f4cf12cff1f66e41c195a3c7b408
y.yanin <yanin.yarosla7@gmail.com>
2023-04-13 22:19:45 +0300

    Созданы классы конфигурации и листнера. При запуске бот может здороваться. Первично немного настроен логгер.

---
+++ src/main/java/com/devpro/shelterBuddyBot/configuration/TelegramBotConfiguration.java
@@ -0,0 +1,15 @@
+package com.devpro.shelterBuddyBot.configuration;
+
+import com.pengrad.telegrambot.TelegramBot;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TelegramBotConfiguration {
+
+    @Bean
+    public TelegramBot telegramBot(@Value("${telegram.bot.token}") String token){
+        return new TelegramBot(token);
+    }
+}
