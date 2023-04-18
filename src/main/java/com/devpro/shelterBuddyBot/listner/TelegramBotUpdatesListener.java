7af81860d1afed32aa8ef72f940c81cd20aeeed4
VladimirSherban <Vovan_Sh_94@mail.ru>
2023-04-19 02:54:09 +0300

    Добавил пока что

--- src/main/java/com/devpro/shelterBuddyBot/listner/TelegramBotUpdatesListener.java
+++ src/main/java/com/devpro/shelterBuddyBot/listner/TelegramBotUpdatesListener.java
@@ -4,6 +4,8 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 import jakarta.annotation.PostConstruct;
@@ -11,13 +13,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
+
 
 @Component
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
     private final TelegramBot telegramBot;
+
 
     public TelegramBotUpdatesListener(TelegramBot telegramBot) {
         this.telegramBot = telegramBot;
@@ -28,7 +33,7 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         telegramBot.setUpdatesListener(this);
     }
 
-    @Override
+
     public int process(List<Update> updates) {
         try {
             updates.forEach(update -> logger.info("Handles update: {}", update));
@@ -37,9 +42,22 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
             Long chatId = message.chat().id();
             String text = message.text();
 
+//            List<InlineKeyboardButton> buttons =  new ArrayList<>();
+//
+//            buttons.add(InlineKeyboardButton.)
+
+            InlineKeyboardMarkup inlineKeyboard = new InlineKeyboardMarkup(
+                    new InlineKeyboardButton("Приют для собак").callbackData("собака"),
+                    new InlineKeyboardButton("Приют для кошек").callbackData("кот"));
+
+
             if ("/start".equals(text)) {
-                SendMessage sendMessage = new SendMessage(chatId, "Hello!");
+                SendMessage sendMessage = new SendMessage(chatId,
+                        "Привет! Я бот помощник. Помогу вам с приютами, выберите какой приют вам нужен: ").replyMarkup(inlineKeyboard);  // описание изменить, это сделал для себя
+
                 SendResponse sendResponse = telegramBot.execute(sendMessage);
+
+
                 if (!sendResponse.isOk()) {
                     logger.error("Error during sending message: {}", sendResponse.description());
                 }
@@ -50,4 +68,5 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
+
 }
