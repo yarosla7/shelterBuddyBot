534e559b040b2b767c09bbcb72a889ea763aa7ad
VladimirSherban <Vovan_Sh_94@mail.ru>
2023-05-05 23:33:31 +0300

    Вернул к состоянию(Все готово по 1 этапу)

--- src/main/java/com/devpro/shelterBuddyBot/exeption/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.devpro.shelterBuddyBot.exeption;
-
-public class ValidationException extends RuntimeException{
-    public ValidationException(String entity) {
-        super("Ошибка валидации сущности " + entity);
-    }
-}
--- src/main/java/com/devpro/shelterBuddyBot/repository/rep/MessageToVolunteerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devpro.shelterBuddyBot.repository.rep;
-
-import com.devpro.shelterBuddyBot.model.enity.MessageToVolunteer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageToVolunteerRepository extends JpaRepository<MessageToVolunteer, Long> {
-}
